ae12d43d45ebcda86543bbcfe6f36a2e5b853460
My Master <derek.straga94@gmail.com>
2020-06-24 20:10:39 -0400

    blah

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,26 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
-
+# Name of User is: ${data.username}
+\
+# Title of Project: ${data.projectTitle}
+\
+# Detail Description: ${data.projectDescription}
+\
+# Steps required to install project: ${data.installationProcess}
+\
+# Instructions for Use: ${data.instruction}
+\
+# Example Instructions: ${data.instructionExample}
+\
+# License Name: ${data.licenseName}
+\
+# License URL: ${data.licenseUrl}
+\
+# Contributor GitHub Usernames: ${data.contributorsGitUserName}
+\
+# Examples on how to run tests: ${data.tests}
+\
+# Questions: ${data.questionsFromUser}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const inquirer = require("inquirer");
-const axios = require("axios");
 const fs = require('fs');
 const path = require('path');
-async function main(){
-    console.log(`starting`);
-    const userResponse = await inquirer
-    .prompt([
+const generateMarkdown = require("./Develop/utils/generateMarkdown");
+
+    const questions = [
         {
             type: "input",
             message: "What is your GitHub user name?",
@@ -55,74 +53,27 @@ async function main(){
             type: "input",
             message: "Provide examples on how to run tests.",
             name: "tests"
+        },
+        {
+            type: "input",
+            message: "Do you have any questions about this application?",
+            name: "questionsFromUser"
         }
-        ]);
-        console.log(`starting`);
-        console.log(userResponse);
-        const gitUsername = userResponse.username;
-        const projectTittle = userResponse.projectTittle;
-        const projectDescription = userResponse.projectDescription;
-        const installationProcess = userResponse.installationProcess;
-        const instruction = userResponse.instruction;
-        const instructionExample = userResponse.instructionExample;
-        const licenseName = userResponse.licenseName;
-        const licenseUrl = userResponse.licenseUrl;
-        const contributorUserNames = userResponse.contributorsGitUserName;
-        const tests = userResponse.tests;
-            // fetching data from git
-            // user
-        const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`);
-        const gitData = gitResponse.data;
-        const gitName = gitData.login;
-        const gitEmail = gitData.email;
-        const gitlocation = gitData.location;
-        const gitUrl = gitData.html_url;
-        const gitProfileImage = gitData.avatar_url;
-            // contributor
-        const contributorUserNamesArray = contributorUserNames.split(",");
-        console.log(contributorUserNamesArray);
-        // const  = listOfContributorsUserNames.
-        // contributorsGitUserName
-        var resultContributor;
-        for (i=0; i<contributorUserNamesArray.length; i++){
-            var contributorsGitUserName = contributorUserNamesArray[i]
-            const gitResponse2 = await axios.get(`https://api.github.com/users/${contributorsGitUserName}`);
-            var gitContribuProfileImage = gitResponse2.data.avatar_url;
-            var gitContribuUrl = gitResponse2.data.html_url;
-            var gitContribuEmail = gitResponse2.data.email;
-            var resultContributor = resultContributor + (`
-            \n <img src="${gitContribuProfileImage}" alt="drawing" width="150" display="inline"/> ${contributorsGitUserName}  GitHubLink: ${gitContribuUrl}`);
-        }
-        var result = (`
-# ${projectTittle} 
-${projectDescription}
-\n* [Installation](#Installation)
-\n* [Instructions](#Instructions)
-\n* [License](#License)
-\n* [Contributors](#Contributors)
-\n* [Author](#Author)
-\n* [Tests](#Tests)
-## Installation
-${installationProcess}
-## Instructions
-${instruction}
-\`\`\`
-${instructionExample}
-\`\`\`
-## License 
-This project is licensed under the ${licenseName} - see the ${licenseUrl} file for details
-## Contributors
-${resultContributor}
-## Tests
-${tests}
-## Author 
-\n![ProfileImage](${gitProfileImage})
-\n**${gitName}**
-\nEmail: ${gitEmail}
-\nLocation:${gitlocation}
-\nGitHub: ${gitUrl}
-`)
-var writeResult = fs.writeFileSync(path.join(__dirname, '../GoodReadMeGenerator', 'readMe.md'), result )
-console.log("file generated....")
-    }
-main();
+        ];
+
+// write a function writeToFile(filename, data)
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+
+
+
+// us passing in questions array and writeToFile = generateMarkdown
+
+function init() {
+    inquirer.prompt(questions).then((questionResponses) => {
+        writeToFile("README.md", generateMarkdown ({ ...questionResponses }));
+    })
+}
+init();
